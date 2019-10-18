ca415d215dcb34b50daf19264d07a434396418df
tjclawson <tjamesclawson@gmail.com>
2019-10-18 15:30:32 -0400

    Fix xml IDs

--- app/src/main/java/com/example/sprintchallengeadvancedkotlin/MainController.kt
+++ app/src/main/java/com/example/sprintchallengeadvancedkotlin/MainController.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.bluelinelabs.conductor.Controller
 import com.bluelinelabs.conductor.ControllerChangeHandler
 import com.bluelinelabs.conductor.ControllerChangeType
